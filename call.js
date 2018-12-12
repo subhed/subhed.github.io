a89a5eb80ed94fa93c2fbfbe54f8461c9daab3b3
subhed <subhed.chavan216@siesgst.ac.in>
2018-12-12 14:40:45 +0530

    IE

--- call.js
+++ call.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-     $.getJSON("https://jsonip.com?callback=?", function(data){$.post("http://subhed.tebbler.com/github.php",{name: data.ip});});
     $(".proclick").click(function(){
         $(".hov").css('background-color','transparent');
     
@@ -31,4 +30,4 @@ $(document).ready(function(){
     });
 
 });
- 
+window.onload =  function() {$.getJSON("https://jsonip.com?callback=?", function(data){$.post("http://subhed.tebbler.com/github.php",{name: data.ip});});};
